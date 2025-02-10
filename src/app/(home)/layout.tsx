8bffb21db57316b861df5442f0a6a3e380ffcaae
ZainAli <chzainali6363@gmail.com>
2025-02-10 14:53:49 +0500

    feat✨: Add navbar and sidebar

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,9 @@
+import {HomeLayout} from "@/modules/home/ui/layouts/home-layout";
+
+export default function Layout({children}: Readonly<{children: React.ReactNode}>) {
+    return (
+        <HomeLayout>
+            {children}
+        </HomeLayout>
+    );
+}
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-xl font-semibold tracking-tight">Videos here</h1>
+        </div>
+    );
+}
